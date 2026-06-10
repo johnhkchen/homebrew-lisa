978deff3c8369e9f236d8a740b5902eb45d30549
John Chen <john.hk.chen@gmail.com>
2026-06-10 12:24:16 -0700

    lisa 0.2.11

--- Formula/lisa.rb
+++ Formula/lisa.rb
@@ -1,35 +1,35 @@
 class Lisa < Formula
   desc "CLI for Lisa DAG-driven concurrent task scheduling"
   homepage "https://github.com/johnhkchen/lisa"
-  version "0.2.10"
+  version "0.2.11"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.10/lisa-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "4851c7b4a78c648b6fd85dc724f8b126b8e3ee404e9f0f316620b4080b32ee51"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.11/lisa-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "57a3c294c33b2525ce7357c897816b5665954dfa73cb9b0595167f2942d4be89"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.10/lisa-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "5fc605d83395687276a2abfe1dc99460f287d1409a339c4884f847c0b6d6048f"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.11/lisa-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "2a4ea16f9669bd0c79d83f3e4bacc1de911d43f25fb8a699d405597628771610"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.10/lisa-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "1932edde55aa606de09965e9740de71fed0ca64993da1a7ea50e40ee6bfd18d1"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.11/lisa-cli-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "9fe25dd42febdc00040e6f5d541941e80a2bdf16295fa1a24fd48b563461d562"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.10/lisa-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "a827fbf47bc52246af78ddafd3e08ed9b9e9f3ecb3916aa75c9cc1512f8b336c"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.11/lisa-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "84ce2faf42d919e2c1471bb36623d5bf0f0b70c5e62e525857173ff690305e4e"
     end
   end
   license "MIT"
 
   BINARY_ALIASES = {
-    "aarch64-apple-darwin":      {},
+    "aarch64-apple-darwin": {},
     "aarch64-unknown-linux-gnu": {},
-    "x86_64-apple-darwin":       {},
-    "x86_64-unknown-linux-gnu":  {},
-  }.freeze
+    "x86_64-apple-darwin": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
 
   def target_triple
     cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
@@ -47,10 +47,18 @@ class Lisa < Formula
   end
 
   def install
-    bin.install "lisa" if OS.mac? && Hardware::CPU.arm?
-    bin.install "lisa" if OS.mac? && Hardware::CPU.intel?
-    bin.install "lisa" if OS.linux? && Hardware::CPU.arm?
-    bin.install "lisa" if OS.linux? && Hardware::CPU.intel?
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "lisa"
+    end
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "lisa"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "lisa"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "lisa"
+    end
 
     install_binary_aliases!
 
