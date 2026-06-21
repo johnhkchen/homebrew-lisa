282b0a5ca8b139fdb1ab53b5a0da1e7cd5c51a28
John Chen <john.hk.chen@gmail.com>
2026-06-20 23:37:39 -0700

    Update lisa to 0.3.0 (manual: HOMEBREW_TAP_TOKEN expired)

--- Formula/lisa.rb
+++ Formula/lisa.rb
@@ -1,25 +1,25 @@
 class Lisa < Formula
   desc "CLI for Lisa DAG-driven concurrent task scheduling"
   homepage "https://github.com/johnhkchen/lisa"
-  version "0.2.12"
+  version "0.3.0"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.12/lisa-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "0c9ef53e3a6e40e4c24f0caba5afc34a9ba7f7b71a785845d14600e062090277"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.3.0/lisa-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "b40834dd99d624b01c2c177948b679cdc5d29367254a4153e6032138158a7069"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.12/lisa-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "f726beacd623e7e74c7b0416ecac93c29d5c3e5db2042a7ec6f1d778e854b913"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.3.0/lisa-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "74385a38fcd0f2095a7ca7146b608e43b2d630347672af60c040081f4e154ab7"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.12/lisa-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "d0253df40b00f122397ccb19828d4624d045646484e8109e31f40b49dc04b7fb"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.3.0/lisa-cli-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "304c5e6e35387ebed5adc292375886ce60a3bf5ad0dfe5a914c9c31c23a0b770"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/johnhkchen/lisa/releases/download/v0.2.12/lisa-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "1563fb5d145eb9533f2e684740cd283218115723612a18f594d2d70cbb3b4c7a"
+      url "https://github.com/johnhkchen/lisa/releases/download/v0.3.0/lisa-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "3c7aa8a69c0cbe8aaea0a384c9551ef800529640feb285865c0b96fcf959137f"
     end
   end
   license "MIT"
